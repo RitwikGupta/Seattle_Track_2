fa9b4f956685dbc54ace718212ea8168f99484e6
William M Boler <wmboler@sei.cmu.edu>
2018-09-23 13:22:29 -0400

    wip: Adding output files from first pass.

--- Code/GeneratePotentialConflicts_BB.py
+++ Code/GeneratePotentialConflicts_BB.py
@@ -224,7 +224,10 @@ def GenerateConflictPairs(boundingBoxParameters, inputFile):
         for j,secondShip in enumerate(boxShipList):
           if i <= j:
             continue
-          print('Pair: ' + str(firstShip) + '\t ' + str(secondShip))
+          print('First: ' + str(firstShip.ship.mmsi) + '\tSecond: ' + str(secondShip.ship.mmsi))
+          if firstShip.ship.mmsi == secondShip.ship.mmsi:
+            continue
+          #print('Pair: ' + str(firstShip) + '\t ' + str(secondShip))
           conflictPair = ConflictPair()
           conflictPair.first = firstShip.ship
           conflictPair.second = secondShip.ship
@@ -237,8 +240,8 @@ def GenerateConflictPairs(boundingBoxParameters, inputFile):
 
   print('Printing ship list: ' + str(len(finalList)))
   
-  for collision in finalList:
-    print(collision)
+  #for collision in finalList:
+  #  print(collision)
           
   print('Completed loading data.')
   
@@ -248,6 +251,56 @@ def GenerateConflictPairs(boundingBoxParameters, inputFile):
     
     
     
+def PrintCollisions(outputFile, collisions):
+  
+  print('Printing collisions: size:' + str(len(collisions)))
+  
+  out = sys.stdout
+  if outputFile != None:
+    out = open(outputFile, 'w')
+      
+  header = ['time_1', 'mmsi_1', 'time_2', 'mmsi_2']
+  with open(outputFile, 'w') as out:
+    csvWriter = csv.writer(out, delimiter=',', lineterminator='\n')
+    csvWriter.writerow(header)
+    for row in collisions:
+      csvWriter.writerow([row.first.basetime, row.first.mmsi, row.second.basetime, row.second.mmsi])
+        
+  if outputFile != None:
+    out.close()
+    
+  print('Printing complete.')
+    
+
+
+def CreateBoundingBoxParameters(inputFileName, latDivisions, lonDivisions, timeDeltaSeconds):
+  
+  bb = BoundingBoxParameters()
+  bb.minLongitude =  float('inf')
+  bb.maxLongitude = -float('inf')
+  bb.minLatitude =   float('inf')
+  bb.maxLatitude =  -float('inf')
+  bb.minTimeSeconds =  float('inf')
+  bb.maxTimeSeconds = -float('inf')
+  bb.latCount = latDivisions
+  bb.lonCount = lonDivisions
+  
+  with open(inputFileName, 'r') as inputFile:
+    csvReader = csv.DictReader(inputFile, delimiter=',')
+    for row in csvReader:
+      bb.minLatitude = min(float(row['LAT']), bb.minLatitude)
+      bb.maxLatitude = max(float(row['LAT']), bb.maxLatitude)
+      bb.minLongitude = min(float(row['LON']), bb.minLongitude)
+      bb.maxLongitude = max(float(row['LON']), bb.maxLongitude)
+      seconds = (pd.to_datetime(row['BaseDateTime'])-datetime.datetime(1970,1,1)).total_seconds()
+      bb.minTimeSeconds = min(float(seconds), bb.minTimeSeconds)
+      bb.maxTimeSeconds = max(float(seconds), bb.maxTimeSeconds)
+      
+    
+  bb.timeCount = (bb.maxTimeSeconds - bb.minTimeSeconds) / (timeDeltaSeconds)
+  
+  return bb
+
     
 def main():
   parser = argparse.ArgumentParser(description='Outputs a list of potential collision pairs based on bounding boxes.')
@@ -256,24 +309,26 @@ def main():
   
   args = parser.parse_args()
   
-  bb = BoundingBoxParameters()
-  bb.minLongitude = -126
-  bb.maxLongitude = -120
-  bb.minLatitude = -90
-  bb.maxLatitude = 90
-  bb.minTimeSeconds = 0
-  bb.maxTimeSeconds = 3944700
-  bb.latCount = 20
-  bb.lonCount = 20
-  bb.timeCount = (bb.maxTimeSeconds - bb.minTimeSeconds) / (30)
+  #bb = BoundingBoxParameters()
+  #bb.minLongitude = -126
+  #bb.maxLongitude = -120
+  #bb.minLatitude = -90
+  #bb.maxLatitude = 90
+  #bb.minTimeSeconds = 0
+  #bb.maxTimeSeconds = 3944700
+  #bb.latCount = 20
+  #bb.lonCount = 20
+  #bb.timeCount = (bb.maxTimeSeconds - bb.minTimeSeconds) / (30)
   
+  bb = CreateBoundingBoxParameters(args.inputFile, 20, 20, 30) # 20 chunks, 20 chunks, 30 seconds
   
   if args.inputFile is None:
     sys.exit('Error, --inputFile not specified.')
     
   print('Loading: ' + args.inputFile)
   
-  GenerateConflictPairs(bb, args.inputFile)
+  collisions = GenerateConflictPairs(bb, args.inputFile)
+  PrintCollisions(args.outputFile, collisions)
 
     
     
